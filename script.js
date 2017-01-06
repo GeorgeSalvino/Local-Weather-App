b8a8e3e8c8b96af5a691c99d0e727a9018ff2fa8
Shiverzz <georgesalvino98@gmail.com>
2017-01-05 23:38:38 -0200

    Local Weather App

---
+++ script.js
@@ -0,0 +1,40 @@
+
+function getWeather(pos) {
+	$.ajax( {
+        //get current position
+	      url: "http://ip-api.com/json",
+	      success: function(data) {
+        //store coordinates in variables
+			  var lat 	=	  data.lat;
+			  var lon 	= 	data.lon;
+        var city  =   data.city;
+        //get weather
+			  $.ajax( {
+	      url: 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&APPID=f6e93916dc3b4a9c1b77dafabc91290a&units=metric',
+	      success: function(data) {
+          //store weather data in variables
+	        var temp      = Math.round(Number(data.main.temp));
+          var weather   = data.weather[0].main;
+          $("body").addClass(weather);
+	        $(".weather").html("<strong><span id=\"temp\">" +temp+"</span>º<span id=\"unit\">C</span><p>"+city);
+	      },
+	      cache: false
+	    })
+	      },
+	    })
+	};
+
+
+getWeather();
+
+
+$(document).on("click","#unit",function(){
+  if($("#unit").text()=="C"){         
+    $("#temp").text(Math.round((Number($("#temp").text())*1.8)+32));
+    $("#unit").text("F");
+  }
+  else{
+    $("#temp").text(Math.round((Number($("#temp").text()) - 32) / 1.8))
+    $("#unit").text("C")
+  }
+})
